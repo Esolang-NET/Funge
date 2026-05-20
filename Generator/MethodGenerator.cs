99d36b1a09e19be4421ae41f7864d5769bd4ed30
juner <juner@juner.net>
2026-05-20 20:36:38 +0900

    インデントや不要なusing の削除や名前の簡略化

--- Generator/MethodGenerator.cs
+++ Generator/MethodGenerator.cs
@@ -2,7 +2,6 @@ using Esolang.Funge.Parser;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Collections.Immutable;
 using System.Text;
 
 namespace Esolang.Funge.Generator;
